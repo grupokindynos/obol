db7978b1bbb10708ed0e543d2ff7958fce44f0ba
Cronos <eabz@polispay.org>
2019-08-23 14:26:03 -0500

    CoinFactory test

---
+++ models/coin-factory/coins_test.go
@@ -0,0 +1,21 @@
+package coin_factory
+
+import (
+	"github.com/grupokindynos/obol/config"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCoinFactory(t *testing.T) {
+	for _, coin := range CoinFactory {
+		newCoin, err := GetCoin(coin.Tag)
+		assert.Nil(t, err)
+		assert.IsType(t, &Coin{}, newCoin)
+	}
+}
+
+func TestNoCoin(t *testing.T) {
+	newCoin, err := GetCoin("NONEXISTINGCOIN")
+	assert.Equal(t, config.ErrorCoinNotAvailable, err)
+	assert.Nil(t, newCoin)
+}

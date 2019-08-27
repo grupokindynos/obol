553f307d4c97280010934be5caecf43915489b04
Cronos <eabz@polispay.org>
2019-08-27 14:37:03 -0500

    Test case #6

--- main_test.go
+++ main_test.go
@@ -38,6 +38,36 @@ func TestSimpleRates(t *testing.T) {
 	}
 }
 
+func TestComplexRates(t *testing.T) {
+	Coins := coinfactory.CoinFactory
+	App := GetApp()
+	for _, coin := range Coins {
+		w := performRequest(App, "GET", "/complex/polis/"+coin.Tag)
+		w2 := performRequest(App, "GET", "/complex/" + coin.Tag + "/polis?amount=1")
+		assert.Equal(t, http.StatusOK, w.Code)
+		assert.Equal(t, http.StatusOK, w2.Code)
+		var firstResponse map[string]interface{}
+		err := json.Unmarshal(w.Body.Bytes(), &firstResponse)
+		assert.Nil(t, err)
+		firstValue, firstExist := firstResponse["status"]
+		assert.True(t, firstExist)
+		assert.Equal(t, float64(1), firstValue)
+		firstResData := firstResponse["data"]
+		assert.NotZero(t, firstResData)
+
+		var secondResponse map[string]interface{}
+		err = json.Unmarshal(w2.Body.Bytes(), &secondResponse)
+		assert.Nil(t, err)
+		secondValue, secondExist := firstResponse["status"]
+		assert.True(t, secondExist)
+		assert.Equal(t, float64(1), secondValue)
+		secondResData := firstResponse["data"]
+		assert.NotZero(t, secondResData)
+
+		assert.Equal(t, firstResData, secondResData)
+	}
+}
+
 func TestNonExistingRoute(t *testing.T) {
 	App := GetApp()
 	w := performRequest(App, "GET", "/none")
